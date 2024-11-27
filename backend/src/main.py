59723e7323a8c1eb6dac868b1c84a2c99bcc116c
Baton <braw31ar@gmail.com>
2024-11-28 03:28:32 +0500

    test not work

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 from backend.src.api.v1.router import  routers
 from backend.src.utils.exception import custom_exception_handler, CustomException
 from backend.src.utils.get_current_user import get_current_user
-from backend.src.work_db import create_tables
-
 #########################
 # BLOCK WITH API ROUTES #
 #########################
@@ -14,11 +14,16 @@ app = FastAPI(title="Twitter",
               debug=True,
               )
 
-
+app.mount("/src/static", StaticFiles(directory="/static"), name="static")
 
 
 app.include_router(routers,dependencies=[Depends(get_current_user)])
 app.add_exception_handler(CustomException,custom_exception_handler)
+
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return HTMLResponse("Hello World!")
 """
 классическая функция для запуска
 """
